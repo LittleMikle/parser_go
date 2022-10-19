5587c0b9706f6e12bbf16b40e8172f34a442d2ed
Zalupessa <little.mikle@yahoo.com>
2022-10-19 21:13:17 +0300

    4 commit

---
+++ db/models.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"gorm.io/gorm"
+)
+
+type Post struct {
+	gorm.Model
+	Title  string  `json:"title" db:"title" gorm:"unique"`
+	Text   string  `json:"text" db:"text"`
+	Images []Image `json:"pets" db:"pets" gorm:"foreignKey:PostID"`
+}
+type Image struct {
+	gorm.Model
+	Name   string `json:"name" db:"name"`
+	PostID int    `json:"post_id" db:"post_id"`
+}
+
+// ErrLogs storage some error logs
+type ErrLogs struct {
+	gorm.Model
+	Error string `json:"error" db:"error"`
+	Place string `json:"place" db:"place"`
+	Count int    `json:"count" db:"count"`
+}
